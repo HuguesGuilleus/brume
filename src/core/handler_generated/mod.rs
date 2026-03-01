e898fa5f45c6759991eb3601c091eb914258f6bd
Hugues GUILLEUS <ghugues@netc.fr>
2026-03-01 21:28:24 +0100

    Add favicon

--- src/core/handler_generated/mod.rs
+++ src/core/handler_generated/mod.rs
@@ -6,11 +6,36 @@ use std::sync::Arc;
 
 use super::{Server, json_handler};
 
+const PAGE404: [u8; 30] = *b"<!DOCTYPE html>404 Not Found\r\n";
+
+const FAVICON_ICO: &[u8] = include_bytes!("favicon.ico");
+const FAVICON_WEBP: &[u8] = include_bytes!("favicon.webp");
+
+pub const MIME_CSS: HeaderValue = HeaderValue::from_static("text/css");
+pub const MIME_ICO: HeaderValue = HeaderValue::from_static("image/vnd.microsoft.icon");
+pub const MIME_WEBP: HeaderValue = HeaderValue::from_static("image/webp");
+pub const MIME_HTML: HeaderValue = HeaderValue::from_static("text/html");
+pub const MIME_JSON: HeaderValue = HeaderValue::from_static("application/json");
+pub const MIME_TEXT: HeaderValue = HeaderValue::from_static("text/plain; charset=UTF-8");
+
 pub fn router() -> Router<Arc<Server>> {
     Router::new()
         .route(
             "/_api.json/{service}",
             routing::post(json_handler).fallback(method_not_allowed),
+        )
+        .route(
+            "/favicon.ico",
+            routing::get(async || ([(axum::http::header::CONTENT_TYPE, MIME_ICO)], FAVICON_ICO)),
+        )
+        .route(
+            "/favicon.webp",
+            routing::get(async || {
+                (
+                    [(axum::http::header::CONTENT_TYPE, MIME_WEBP)],
+                    FAVICON_WEBP,
+                )
+            }),
         )
         .fallback(method_not_allowed)
         .fallback(routing::get(serve_generated))
@@ -23,13 +48,6 @@ async fn method_not_allowed() -> impl axum::response::IntoResponse {
         "405 Method Not Allowed\r\n",
     )
 }
-
-pub const MIME_CSS: HeaderValue = HeaderValue::from_static("text/css");
-pub const MIME_HTML: HeaderValue = HeaderValue::from_static("text/html");
-pub const MIME_JSON: HeaderValue = HeaderValue::from_static("application/json");
-pub const MIME_TEXT: HeaderValue = HeaderValue::from_static("text/plain; charset=UTF-8");
-
-const PAGE404: [u8; 30] = *b"<!DOCTYPE html>404 Not Found\r\n";
 
 async fn serve_generated(
     axum::extract::State(server): axum::extract::State<Arc<Server>>,
